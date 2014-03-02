1b41e666fe33eed5397bbf1bcb6a27bac8058224
Daniel Harlea <daniel.harlea@gmail.com>
2014-03-02 17:28:58 +0100

    Updated .gitignore Added App.js, status route and config

---
+++ app.js
@@ -0,0 +1,27 @@
+var express	= require('express'),
+  http		= require('http'),
+  path		= require('path'),
+  status	= require('./routes/status');
+
+var config = require('./config/config');
+
+var app = express();
+app.set('port', process.env.PORT || 7777);
+app.set('jsonp callback name', 'callback');
+app.set('env', 'development');
+
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.use(express.logger('dev'));
+app.use(express.compress());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.favicon());
+
+app.get(config.prefix + '/status', status.state);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ config/config.js
@@ -0,0 +1,5 @@
+var config = {};
+
+config.prefix = '/node';
+
+module.exports = config;
