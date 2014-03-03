b6ccc55688ec437018564eac49d151f320e9c878
Daniel Harlea <daniel.harlea@gmail.com>
2014-03-03 15:12:58 +0100

    - added more info about the request & server in the response

--- routes/status.js
+++ routes/status.js
@@ -1,12 +1,25 @@
-var url = require('url');
+var url = require('url'),
+	os = require('os');
 
 exports.state = function(request, response) {
   var parsedUrl = url.parse(request.url, true);
   var _queryObj = parsedUrl.query;
 
-  console.log(JSON.stringify(_queryObj));
+  var os		= {};
+  os.hostname	= os.hostname();
+  os.type		= os.type();
+  os.platform	= os.platform();
+  os.release	= os.release();
+  os.uptime		= os.uptime();
+  os.loadavg	= os.loadavg();
+  os.totalmem	= os.totalmem();
+  os.freemem	= os.freemem();
+  os.cpus		= os.cpus();
 
-  var result = {};
-  result.success = true;
+  var result	= {};
+  result.success= true;
+  result.query	= JSON.stringify(_queryObj);
+  result.request= JSON.stringify(request);
+  
   response.send(result);
 };
