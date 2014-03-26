f44fcdd559561c410039fe3c0aeb577cce4ead4f
Daniel Harlea <daniel.harlea@gmail.com>
2014-03-26 15:57:32 +0100

    - updated response info

--- routes/status.js
+++ routes/status.js
@@ -1,32 +1,42 @@
-var url	= require('url'),
-	os	= require('os');
+var url = require('url')
+	, os = require('os');
 
 exports.state = function(request, response) {
-  var os_info		= {};
-  os_info.hostname	= os.hostname();
-  os_info.type		= os.type();
-  os_info.platform	= os.platform();
-  os_info.arch		= os.arch();
-  os_info.release	= os.release();
-  os_info.uptime	= os.uptime();
-  os_info.loadavg	= os.loadavg();
-  os_info.totalmem	= os.totalmem();
-  os_info.freemem	= os.freemem();
-  os_info.cpus		= os.cpus();
-  os_info.nw_if		= os.networkInterfaces();
-  os_info.EOL		= os.EOL;
+	var result = {};
+	try {
+		var os_info = {};
+		os_info.hostname = os.hostname();
+		os_info.type = os.type();
+		os_info.platform = os.platform();
+		os_info.arch = os.arch();
+		os_info.release = os.release();
+		os_info.uptime = os.uptime();
+		os_info.loadavg = os.loadavg();
+		os_info.totalmem = os.totalmem();
+		os_info.freemem = os.freemem();
+		os_info.cpus = os.cpus();
+		os_info.nw_if = os.networkInterfaces();
+		os_info.EOL = os.EOL;
 
-  var parsedUrl		= url.parse(request.url, true);
-  var queryObj		= parsedUrl.query;
+		var parsedUrl = url.parse(request.url, true);
+		var queryObj = parsedUrl.query;
 
-  var conn			= {};
-  conn.address		= request.connection.remoteAddress;
+		var conn = {};
+		conn.address = request.connection.remoteAddress;
+		conn.port = request.app.settings.port;
 
-  var result		= {};
-  result.success	= true;
-  result.query		= JSON.stringify(queryObj);
-  result.conn		= conn;
-  result.os_info	= os_info;
+		result.success = true;
+		result.timestamp = new Date().getTime();
+		result.date = new Date();
+		result.query = JSON.stringify(queryObj);
+		result.href = "http://" + conn.address + ":" + conn.port + request.url;
+		result.conn = conn;
+		result.os_info = os_info;
 
-  response.send(200, result);
+		response.send(200, result);
+	} catch (err) {
+		result.success = false;
+		result.error = err;
+		response.send(500, result);
+	}
 };
